0bd64336948e9c251c66d323b0737f0df8278aa5
Levartar <jakob.guenster@gmx.net>
2025-06-24 05:57:29 +0200

    add end to procedural level

--- levels/procedural_map.gd
+++ levels/procedural_map.gd
@@ -4,9 +4,13 @@ extends Node2D
 @onready var tileset:TileSet = tilemap.tile_set
 @onready var current_x = 12
 
+const length = 15
+
 func _ready():
-	for i in range(50):
-		var pattern = tileset.get_pattern(randi() % tileset.get_patterns_count())
+	for i in range(length):
+		var pattern = tileset.get_pattern(randi() % (tileset.get_patterns_count()-1))
+		if i+1 == length:
+			pattern = tileset.get_pattern(tileset.get_patterns_count()-1)
 		tilemap.set_pattern(Vector2i(current_x, -16), pattern)
 
 		var width = pattern.get_size().x
